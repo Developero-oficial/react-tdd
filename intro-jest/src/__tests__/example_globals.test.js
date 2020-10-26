4a66fd8bf6771c258b31dda6fdaf7c450e861fd3
juan-carlos-correa <juan.correa.herrera@gmail.com>
2020-10-26 08:14:59 -0600

    added example_globals

---
+++ intro-jest/src/__tests__/example_globals.test.js
@@ -0,0 +1,13 @@
+describe('this is for group test cases', () => {
+  it('description', () => {
+    expect(true).toBe(true);
+  });
+
+  it('description 2', () => {
+    expect(true).toBe(true);
+  });
+
+  it('description 3', () => {
+    expect(true).toBe(true);
+  });
+});
