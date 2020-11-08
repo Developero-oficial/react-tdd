e0ae6b3bcaac0ced25db6fa7700a144ccddb541a
juan-carlos-correa <juan.correa.herrera@gmail.com>
2020-11-08 10:19:18 -0600

    store-app/form-inputs-test

--- store-app/src/form/form.js
+++ store-app/src/form/form.js
@@ -1,5 +1,34 @@
 import React from 'react'
+import TextField from '@material-ui/core/TextField'
+import InputLabel from '@material-ui/core/InputLabel'
+import Select from '@material-ui/core/Select'
 
-export const Form = () => <h1>Create product</h1>
+export const Form = () => (
+  <>
+    <h1>Create product</h1>
+
+    <form>
+      <TextField label="name" id="name" />
+
+      <TextField label="size" id="size" />
+
+      <InputLabel htmlFor="type">Type</InputLabel>
+
+      <Select
+        native
+        value=""
+        inputProps={{
+          name: 'type',
+          id: 'type',
+        }}
+      >
+        <option aria-label="None" value="" />
+        <option value="electronic">Electronic</option>
+        <option value="furniture">Furniture</option>
+        <option value="clothing">Clothing</option>
+      </Select>
+    </form>
+  </>
+)
 
 export default Form
--- store-app/src/form/form.test.js
+++ store-app/src/form/form.test.js
@@ -4,10 +4,21 @@ import {screen, render} from '@testing-library/react'
 import {Form} from './form'
 
 describe('when the form is mounted', () => {
+  beforeEach(() => render(<Form />))
+
   it('there must be a create product form page', () => {
-    render(<Form />)
     expect(
       screen.getByRole('heading', {name: /create product/i}),
     ).toBeInTheDocument()
+  })
+
+  it('should exists the fields: name, size, type (electronic, furniture, clothing)', () => {
+    expect(screen.getByLabelText(/name/i)).toBeInTheDocument()
+    expect(screen.getByLabelText(/size/i)).toBeInTheDocument()
+    expect(screen.getByLabelText(/type/i)).toBeInTheDocument()
+
+    expect(screen.queryByText(/electronic/i)).toBeInTheDocument()
+    expect(screen.queryByText(/furniture/i)).toBeInTheDocument()
+    expect(screen.queryByText(/clothing/i)).toBeInTheDocument()
   })
 })
