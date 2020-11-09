6049655700af399d31a4a2f4f6d4a43ea0d0d6b0
juan-carlos-correa <juan.correa.herrera@gmail.com>
2020-11-09 14:22:39 -0600

    store-app/ui structure and styles

--- store-app/src/form/form.js
+++ store-app/src/form/form.js
@@ -3,6 +3,10 @@ import TextField from '@material-ui/core/TextField'
 import InputLabel from '@material-ui/core/InputLabel'
 import Select from '@material-ui/core/Select'
 import Button from '@material-ui/core/Button'
+import Container from '@material-ui/core/Container'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import Grid from '@material-ui/core/Grid'
+import Typography from '@material-ui/core/Typography'
 
 import {saveProduct} from '../services/productServices'
 import {
@@ -88,52 +92,72 @@ export const Form = () => {
   }
 
   return (
-    <>
-      <h1>Create product</h1>
+    <Container maxWidth="xs">
+      <CssBaseline />
+
+      <Typography component="h1" variant="h5" align="center">
+        Create product
+      </Typography>
 
       {isSuccess && <p>Product Stored</p>}
 
       <p>{errorMessage}</p>
 
       <form onSubmit={handleSubmit}>
-        <TextField
-          label="name"
-          id="name"
-          name="name"
-          helperText={formErrors.name}
-          onBlur={handleBlur}
-        />
+        <Grid container spacing={4}>
+          <Grid item xs={12}>
+            <TextField
+              fullWidth
+              label="name"
+              id="name"
+              name="name"
+              helperText={formErrors.name}
+              onBlur={handleBlur}
+              error={!!formErrors.name.length}
+            />
+          </Grid>
 
-        <TextField
-          label="size"
-          id="size"
-          name="size"
-          helperText={formErrors.size}
-          onBlur={handleBlur}
-        />
+          <Grid item xs={12}>
+            <TextField
+              fullWidth
+              label="size"
+              id="size"
+              name="size"
+              helperText={formErrors.size}
+              onBlur={handleBlur}
+              error={!!formErrors.size.length}
+            />
+          </Grid>
 
-        <InputLabel htmlFor="type">Type</InputLabel>
+          <Grid item xs={12}>
+            <InputLabel htmlFor="type">Type</InputLabel>
 
-        <Select
-          native
-          inputProps={{
-            name: 'type',
-            id: 'type',
-          }}
-        >
-          <option aria-label="None" value="" />
-          <option value="electronic">Electronic</option>
-          <option value="furniture">Furniture</option>
-          <option value="clothing">Clothing</option>
-        </Select>
+            <Select
+              native
+              fullWidth
+              error={!!formErrors.type.length}
+              inputProps={{
+                name: 'type',
+                id: 'type',
+              }}
+            >
+              <option aria-label="None" value="" />
+              <option value="electronic">Electronic</option>
+              <option value="furniture">Furniture</option>
+              <option value="clothing">Clothing</option>
+            </Select>
 
-        {formErrors.type.length && <p>{formErrors.type}</p>}
+            {!!formErrors.type && <p>{formErrors.type}</p>}
+          </Grid>
 
-        <Button disabled={isSaving} type="submit">
-          Submit
-        </Button>
+          <Grid item xs={12}>
+            <Button fullWidth disabled={isSaving} type="submit">
+              Submit
+            </Button>
+          </Grid>
+        </Grid>
       </form>
-    </>
+    </Container>
   )
 }
 
