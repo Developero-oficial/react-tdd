2293afaeb6d2cd0f3b95aeb304b3f30e8328a0a9
juan-carlos-correa <juan.correa.herrera@gmail.com>
2020-11-14 07:28:33 -0600

    github-search/filters layout structure

--- github-search/src/components/github-search-page/github-search-page.js
+++ github-search/src/components/github-search-page/github-search-page.js
@@ -2,17 +2,27 @@ import React from 'react'
 import Typography from '@material-ui/core/Typography'
 import TextField from '@material-ui/core/TextField'
 import Button from '@material-ui/core/Button'
+import Container from '@material-ui/core/Container'
+import Grid from '@material-ui/core/Grid'
 
 export const GithubSearchPage = () => (
-  <>
+  <Container>
     <Typography variant="h3" component="h1">
       Github repositories list page
     </Typography>
 
-    <TextField label="Filter by" id="filterBy" />
+    <Grid container spacing={2} justify="space-between">
+      <Grid item md={6} xs={12}>
+        <TextField fullWidth label="Filter by" id="filterBy" />
+      </Grid>
 
-    <Button>Search</Button>
-  </>
+      <Grid item md={3} xs={12}>
+        <Button fullWidth color="primary" variant="contained">
+          Search
+        </Button>
+      </Grid>
+    </Grid>
+  </Container>
 )
 
 export default GithubSearchPage
