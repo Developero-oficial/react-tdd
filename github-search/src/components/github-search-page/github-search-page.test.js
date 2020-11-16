8a61d7a9e9cb07727ed9b02636ac380d9126ef64
juan-carlos-correa <juan.correa.herrera@gmail.com>
2020-11-16 16:55:57 -0600

    github-search/test previous page

--- github-search/src/components/github-search-page/github-search-page.test.js
+++ github-search/src/components/github-search-page/github-search-page.test.js
@@ -297,5 +297,45 @@ describe('when the developer clicks on search and then on next page button', () 
     )
 
     expect(screen.getByRole('cell', {name: /2-0/})).toBeInTheDocument()
-  }, 10000)
+  }, 30000)
+})
+
+describe('when the developer clicks on search and then on next page button and then on previous page button', () => {
+  it('must display the previous repositories page', async () => {
+    server.use(rest.get('/search/repositories', handlerPaginated))
+
+    fireClickSearch()
+
+    expect(await screen.findByRole('table')).toBeInTheDocument()
+
+    expect(screen.getByRole('cell', {name: /1-0/})).toBeInTheDocument()
+
+    expect(screen.getByRole('button', {name: /next page/i})).not.toBeDisabled()
+
+    fireEvent.click(screen.getByRole('button', {name: /next page/i}))
+
+    expect(screen.getByRole('button', {name: /search/i})).toBeDisabled()
+
+    await waitFor(
+      () =>
+        expect(
+          screen.getByRole('button', {name: /search/i}),
+        ).not.toBeDisabled(),
+      {timeout: 3000},
+    )
+
+    expect(screen.getByRole('cell', {name: /2-0/})).toBeInTheDocument()
+
+    fireEvent.click(screen.getByRole('button', {name: /previous page/i}))
+
+    await waitFor(
+      () =>
+        expect(
+          screen.getByRole('button', {name: /search/i}),
+        ).not.toBeDisabled(),
+      {timeout: 3000},
+    )
+
+    expect(screen.getByRole('cell', {name: /1-0/})).toBeInTheDocument()
+  }, 30000)
 })
