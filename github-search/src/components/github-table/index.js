f7cbe06a4e62ec726bd7fdb3ad77acfcf9f178fb
juan-carlos-correa <juan.correa.herrera@gmail.com>
2020-11-16 16:15:31 -0600

    github-search/make sticky header table

--- github-search/src/components/github-table/index.js
+++ github-search/src/components/github-table/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import {makeStyles} from '@material-ui/core/styles'
 import Table from '@material-ui/core/Table'
 import TableBody from '@material-ui/core/TableBody'
 import TableCell from '@material-ui/core/TableCell'
@@ -17,44 +18,54 @@ const tableHeaders = [
   'Updated at',
 ]
 
-export const GithubTable = ({reposList}) => (
-  <TableContainer>
-    <Table>
-      <TableHead>
-        <TableRow>
-          {tableHeaders.map(name => (
-            <TableCell key={name}>{name}</TableCell>
-          ))}
-        </TableRow>
-      </TableHead>
-      <TableBody>
-        {reposList.map(
-          ({
-            name,
-            id,
-            stargazers_count: stargazersCount,
-            forks_count: forksCount,
-            open_issues_count: openIssuesCount,
-            updated_at: updatedAt,
-            html_url: htmlUrl,
-            owner: {avatar_url: avatarUrl},
-          }) => (
-            <TableRow key={id}>
-              <TableCell>
-                <Avatar alt={name} src={avatarUrl} />
-                <Link href={htmlUrl}>{name}</Link>
-              </TableCell>
-              <TableCell>{stargazersCount}</TableCell>
-              <TableCell>{forksCount}</TableCell>
-              <TableCell>{openIssuesCount}</TableCell>
-              <TableCell>{updatedAt}</TableCell>
-            </TableRow>
-          ),
-        )}
-      </TableBody>
-    </Table>
-  </TableContainer>
-)
+const useStyles = makeStyles({
+  container: {
+    maxHeight: 440,
+  },
+})
+
+export const GithubTable = ({reposList}) => {
+  const classes = useStyles()
+
+  return (
+    <TableContainer className={classes.container}>
+      <Table stickyHeader>
+        <TableHead>
+          <TableRow>
+            {tableHeaders.map(name => (
+              <TableCell key={name}>{name}</TableCell>
+            ))}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {reposList.map(
+            ({
+              name,
+              id,
+              stargazers_count: stargazersCount,
+              forks_count: forksCount,
+              open_issues_count: openIssuesCount,
+              updated_at: updatedAt,
+              html_url: htmlUrl,
+              owner: {avatar_url: avatarUrl},
+            }) => (
+              <TableRow key={id}>
+                <TableCell>
+                  <Avatar alt={name} src={avatarUrl} />
+                  <Link href={htmlUrl}>{name}</Link>
+                </TableCell>
+                <TableCell>{stargazersCount}</TableCell>
+                <TableCell>{forksCount}</TableCell>
+                <TableCell>{openIssuesCount}</TableCell>
+                <TableCell>{updatedAt}</TableCell>
+              </TableRow>
+            ),
+          )}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  )
+}
 
 export default GithubTable
 
