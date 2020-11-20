2562e8c8e61d2038a14c5ff9effb2ab958361fc3
juan-carlos-correa <juan.correa.herrera@gmail.com>
2020-11-20 15:46:22 -0600

    auth-company/login inputs and button added

--- auth-company/src/auth/components/login-page/login-page.js
+++ auth-company/src/auth/components/login-page/login-page.js
@@ -1,5 +1,14 @@
 import React from 'react'
+import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button'
 
-export const LoginPage = () => <h1>Login Page</h1>
+export const LoginPage = () => (
+  <>
+    <h1>Login Page</h1>
+    <TextField label="email" id="email" />
+    <TextField label="password" id="password" type="password" />
+    <Button>Send</Button>
+  </>
+)
 
 export default {LoginPage}
--- auth-company/src/auth/components/login-page/login-page.test.js
+++ auth-company/src/auth/components/login-page/login-page.test.js
@@ -3,9 +3,16 @@ import {screen, render} from '@testing-library/react'
 
 import {LoginPage} from './login-page'
 
+beforeEach(() => render(<LoginPage />))
+
 describe('when login page is mounted', () => {
   it('must display the login title', () => {
-    render(<LoginPage />)
     expect(screen.getByText(/login page/i)).toBeInTheDocument()
+  })
+
+  it('must have a form with the following fields: email, password and a submit button', () => {
+    expect(screen.getByLabelText(/email/i)).toBeInTheDocument()
+    expect(screen.getByLabelText(/password/i)).toBeInTheDocument()
+    expect(screen.getByRole('button', {name: /send/i}))
   })
 })
