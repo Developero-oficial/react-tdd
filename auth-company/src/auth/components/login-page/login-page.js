1eb150cfe729d5abd6d6cebcde49538b3d852e29
juan-carlos-correa <juan.correa.herrera@gmail.com>
2020-11-21 15:36:09 -0600

    auth-company/disable send button on fetch

--- auth-company/src/auth/components/login-page/login-page.js
+++ auth-company/src/auth/components/login-page/login-page.js
@@ -21,8 +21,9 @@ export const LoginPage = () => {
   const [emailValidationMessage, setEmailValidationMessage] = useState('')
   const [passwordValidationMessage, setPasswordValidationMessage] = useState('')
   const [formValues, setFormValues] = useState({email: '', password: ''})
+  const [isFetching, setIsFetching] = useState(false)
 
-  const handleSubmit = e => {
+  const handleSubmit = async e => {
     e.preventDefault()
     const {email, password} = e.target.elements
 
@@ -33,6 +34,12 @@ export const LoginPage = () => {
     if (!password.value) {
       setPasswordValidationMessage('The password is required')
     }
+
+    setIsFetching(true)
+
+    await fetch('/login', {method: 'POST'})
+
+    setIsFetching(false)
   }
 
   const handleChange = ({target: {value, name}}) => {
@@ -82,7 +89,9 @@ export const LoginPage = () => {
           onBlur={handleBlurPassword}
           value={formValues.password}
         />
-        <Button type="submit">Send</Button>
+        <Button disabled={isFetching} type="submit">
+          Send
+        </Button>
       </form>
     </>
   )
--- auth-company/src/auth/components/login-page/login-page.test.js
+++ auth-company/src/auth/components/login-page/login-page.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {screen, render, fireEvent} from '@testing-library/react'
+import {screen, render, fireEvent, waitFor} from '@testing-library/react'
 import {setupServer} from 'msw/node'
 
 import {LoginPage} from './login-page'
@@ -9,6 +9,8 @@ const passwordValidationMessage =
   'The password must contain at least 8 characters, one upper case letter, one number and one special character'
 
 const getPasswordInput = () => screen.getByLabelText(/password/i)
+
+const getSendButton = () => screen.getByRole('button', {name: /send/i})
 
 const server = setupServer(...handlers)
 
@@ -28,35 +30,39 @@ describe('when login page is mounted', () => {
   it('must have a form with the following fields: email, password and a submit button', () => {
     expect(screen.getByLabelText(/email/i)).toBeInTheDocument()
     expect(screen.getByLabelText(/password/i)).toBeInTheDocument()
-    expect(screen.getByRole('button', {name: /send/i}))
+    expect(getSendButton())
   })
 })
 
 describe('when the user leaves empty fields and clicks the submit button', () => {
-  it('display required messages as the format: "The [field name] is required"', () => {
+  it('display required messages as the format: "The [field name] is required"', async () => {
     expect(screen.queryByText(/the email is required/i)).not.toBeInTheDocument()
     expect(
       screen.queryByText(/the password is required/i),
     ).not.toBeInTheDocument()
 
-    fireEvent.click(screen.getByRole('button', {name: /send/i}))
+    fireEvent.click(getSendButton())
 
     expect(screen.getByText(/the email is required/i)).toBeInTheDocument()
     expect(screen.getByText(/the password is required/i)).toBeInTheDocument()
+
+    await waitFor(() => expect(getSendButton()).not.toBeDisabled())
   })
 })
 
 describe('when the user fills the fields and clicks the submit button', () => {
-  it('must not display the required messages', () => {
+  it('must not display the required messages', async () => {
     screen.getByLabelText(/email/i).value = 'john.doe@test.com'
     screen.getByLabelText(/password/i).value = 'Aa123456789!@#'
 
-    fireEvent.click(screen.getByRole('button', {name: /send/i}))
+    fireEvent.click(getSendButton())
 
     expect(screen.queryByText(/the email is required/i)).not.toBeInTheDocument()
     expect(
       screen.queryByText(/the password is required/i),
     ).not.toBeInTheDocument()
+
+    await waitFor(() => expect(getSendButton()).not.toBeDisabled())
   })
 })
 
@@ -151,9 +157,13 @@ then change with valid value and blur again`, () => {
 })
 
 describe('when the user submit the login form with valid data', () => {
-  it.todo(
-    'must disable the submit button while the form page is fetching the data',
-  )
+  it('must disable the submit button while the form page is fetching the data', async () => {
+    fireEvent.click(getSendButton())
+
+    expect(getSendButton()).toBeDisabled()
+
+    await waitFor(() => expect(getSendButton()).not.toBeDisabled())
+  })
 
   it.todo(
     'must be a loading indicator at the top of the form while it is fetching',
