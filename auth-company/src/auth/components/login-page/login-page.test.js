1c00ae61f6c4f111ea3906cae48ff83ec1d0f5e8
juan-carlos-correa <juan.correa.herrera@gmail.com>
2020-11-22 14:58:48 -0600

    auth-company/refactor test invalid credentials

--- auth-company/src/auth/components/login-page/login-page.test.js
+++ auth-company/src/auth/components/login-page/login-page.test.js
@@ -10,11 +10,8 @@ import {setupServer} from 'msw/node'
 import {rest} from 'msw'
 
 import {LoginPage} from './login-page'
-import {handlers} from '../../../mocks/handlers'
-
-const HTTP_UNEXPECTED_ERROR_STATUS = 500
-const HTTP_INVALID_CREDENTIALS_STATUS = 401
-const HTTP_OK_STATUS = 200
+import {handlers, handlerInvalidCredentials} from '../../../mocks/handlers'
+import {HTTP_UNEXPECTED_ERROR_STATUS} from '../../../consts'
 
 const passwordValidationMessage =
   'The password must contain at least 8 characters, one upper case letter, one number and one special character'
@@ -231,27 +228,18 @@ describe('when the user submit the login form with valid data and there is an un
 
 describe('when the user submit the login form with valid data and there is an invalid credentials error', () => {
   it('must display the error message "The email or password are not correct" from the api', async () => {
-    // setup server
-    server.use(
-      rest.post('/login', (req, res, ctx) => {
-        const {email, password} = req.body
+    const wrongEmail = 'wrong@mail.com'
+    const wrongPassword = 'Aa12345678$'
 
-        if (email === 'wrong@mail.com' && password === 'Aa12345678$') {
-          return res(
-            ctx.status(HTTP_INVALID_CREDENTIALS_STATUS),
-            ctx.json({message: 'The email or password are not correct'}),
-          )
-        }
+    server.use(handlerInvalidCredentials({wrongEmail, wrongPassword}))
 
-        return res(ctx.status(HTTP_OK_STATUS))
-      }),
-    )
+    expect(
+      screen.queryByText(/the email or password are not correct/i),
+    ).not.toBeInTheDocument()
 
-    // trigger - submit form
-    fillInputs({email: 'wrong@mail.com', password: 'Aa12345678$'})
+    fillInputs({email: wrongEmail, password: wrongPassword})
     fireEvent.click(getSendButton())
 
-    // expects error message
     expect(
       await screen.findByText(/the email or password are not correct/i),
     ).toBeInTheDocument()
--- auth-company/src/mocks/handlers.js
+++ auth-company/src/mocks/handlers.js
@@ -1,6 +1,8 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import {rest} from 'msw'
 
+import {HTTP_INVALID_CREDENTIALS_STATUS, HTTP_OK_STATUS} from '../consts'
+
 export const handlers = [
   rest.post('/login', (req, res, ctx) => {
     sessionStorage.setItem('is-authenticated', true)
@@ -8,4 +10,18 @@ export const handlers = [
   }),
 ]
 
-export default {handlers}
+export const handlerInvalidCredentials = ({wrongPassword, wrongEmail}) =>
+  rest.post('/login', (req, res, ctx) => {
+    const {email, password} = req.body
+
+    if (email === wrongEmail && password === wrongPassword) {
+      return res(
+        ctx.status(HTTP_INVALID_CREDENTIALS_STATUS),
+        ctx.json({message: 'The email or password are not correct'}),
+      )
+    }
+
+    return res(ctx.status(HTTP_OK_STATUS))
+  })
+
+export default {handlers, handlerInvalidCredentials}
