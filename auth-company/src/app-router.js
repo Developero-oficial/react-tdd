1189dcafe85878c7473a5562d4e890143efe4f60
juan-carlos-correa <juan.correa.herrera@gmail.com>
2020-11-23 07:34:24 -0600

    auth-company/implement authenticated user test

--- auth-company/src/app-router.js
+++ auth-company/src/app-router.js
@@ -8,32 +8,39 @@ const AdminPage = () => <h1>Admin page</h1>
 
 const EmployeePage = () => <h1>Admin page</h1>
 
-const isAuth = false
-
-const PrivateRoute = ({children, path}) => (
+const PrivateRoute = ({children, path, isAuth}) => (
   <Route path={path} exact>
-    {isAuth ? {children} : <Redirect to="/" />}
+    {isAuth ? children : <Redirect to="/" />}
   </Route>
 )
 
 PrivateRoute.propTypes = {
   children: PropTypes.node.isRequired,
   path: PropTypes.string.isRequired,
+  isAuth: PropTypes.bool.isRequired,
 }
 
-export const AppRouter = () => (
+export const AppRouter = ({isAuth}) => (
   <Switch>
     <Route path="/" exact>
       <LoginPage />
     </Route>
-    <PrivateRoute path="/admin">
+    <PrivateRoute path="/admin" isAuth={isAuth}>
       <AdminPage />
     </PrivateRoute>
-    <PrivateRoute path="/employee">
+    <PrivateRoute path="/employee" isAuth={isAuth}>
       <EmployeePage />
     </PrivateRoute>
   </Switch>
 )
+
+AppRouter.propTypes = {
+  isAuth: PropTypes.bool,
+}
+
+AppRouter.defaultProps = {
+  isAuth: false,
+}
 
 export default {
   AppRouter,
--- auth-company/src/app.router.test.js
+++ auth-company/src/app.router.test.js
@@ -19,3 +19,11 @@ describe('when the user is not authenticated and enters on employee page', () =>
     expect(screen.getByText(/login page/i)).toBeInTheDocument()
   })
 })
+
+describe('when the user is authenticated and enters on admin page', () => {
+  it('must be redirected to login page', () => {
+    renderWithRouter(<AppRouter isAuth />, {route: '/admin'})
+
+    expect(screen.getByText(/admin page/i)).toBeInTheDocument()
+  })
+})
