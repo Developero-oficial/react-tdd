43e21b9b0b1b433c15ef297154f8c72103d3caba
juan-carlos-correa <juan.correa.herrera@gmail.com>
2020-11-26 12:51:11 -0600

    auth-company/display delete button to admin user only

--- auth-company/src/employee/components/employee-page/employee-page.js
+++ auth-company/src/employee/components/employee-page/employee-page.js
@@ -1,5 +1,17 @@
-import React from 'react'
+import React, {useContext} from 'react'
 
-export const EmployeePage = () => <h1>Employee page</h1>
+import {AuthContext} from '../../../utils/contexts/auth-context'
+
+export const EmployeePage = () => {
+  const {user} = useContext(AuthContext)
+
+  return (
+    <>
+      <h1>Employee page</h1>
+
+      {user.role === 'admin' && <button type="button">Delete</button>}
+    </>
+  )
+}
 
 export default {EmployeePage}
