7f335e0c08a0608b85e1567a2b90d606973bfef1
juan-carlos-correa <juan.correa.herrera@gmail.com>
2020-11-26 13:05:56 -0600

    auth-company/refactor employee test

--- auth-company/src/consts/index.js
+++ auth-company/src/consts/index.js
@@ -3,10 +3,12 @@ export const HTTP_INVALID_CREDENTIALS_STATUS = 401
 export const HTTP_OK_STATUS = 200
 
 export const ADMIN_ROLE = 'admin'
+export const EMPLOYEE_ROLE = 'employee'
 
 export default {
   HTTP_UNEXPECTED_ERROR_STATUS,
   HTTP_INVALID_CREDENTIALS_STATUS,
   HTTP_OK_STATUS,
   ADMIN_ROLE,
+  EMPLOYEE_ROLE,
 }
--- auth-company/src/employee/components/employee-page/employee-page.test.js
+++ auth-company/src/employee/components/employee-page/employee-page.test.js
@@ -3,16 +3,18 @@ import {render, screen} from '@testing-library/react'
 
 import {EmployeePage} from './employee-page'
 import {AuthContext} from '../../../utils/contexts/auth-context'
+import {ADMIN_ROLE, EMPLOYEE_ROLE} from '../../../consts'
+
+const renderWith = ({role, username = 'John Doe'}) =>
+  render(
+    <AuthContext.Provider value={{user: {username, role}}}>
+      <EmployeePage />
+    </AuthContext.Provider>,
+  )
 
 describe('when the admin access to employee page', () => {
   it('must have access to delete the employee button', () => {
-    render(
-      <AuthContext.Provider
-        value={{user: {username: 'John Doe', role: 'admin'}}}
-      >
-        <EmployeePage />
-      </AuthContext.Provider>,
-    )
+    renderWith({role: ADMIN_ROLE})
 
     expect(screen.getByRole('button', {name: /delete/i})).toBeInTheDocument()
   })
@@ -20,13 +22,7 @@ describe('when the admin access to employee page', () => {
 
 describe('when the employee access to employee page', () => {
   it('must not have access to delete the employee button', () => {
-    render(
-      <AuthContext.Provider
-        value={{user: {username: 'John Doe', role: 'employee'}}}
-      >
-        <EmployeePage />
-      </AuthContext.Provider>,
-    )
+    renderWith({role: EMPLOYEE_ROLE})
 
     expect(
       screen.queryByRole('button', {name: /delete/i}),
