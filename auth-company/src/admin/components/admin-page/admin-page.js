07fde6378e0e0176c7b1daf7dd05fc3874a2a8fb
juan-carlos-correa <juan.correa.herrera@gmail.com>
2020-11-26 12:08:25 -0600

    auth-company/refactor admin page ui

--- auth-company/src/admin/components/admin-page/admin-page.js
+++ auth-company/src/admin/components/admin-page/admin-page.js
@@ -1,13 +1,24 @@
 import React, {useContext} from 'react'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
 
 import {AuthContext} from '../../../utils/contexts/auth-context'
 
 export const AdminPage = () => {
   const {user} = useContext(AuthContext)
+
   return (
     <>
-      <h1>Admin page</h1>
-      <p>{user.username}</p>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6">{user.username}</Typography>
+        </Toolbar>
+      </AppBar>
+
+      <Typography component="h1" variant="h5">
+        Admin page
+      </Typography>
     </>
   )
 }
